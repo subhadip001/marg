eb18f3a89c3457a54579ead9f0ffbb42dd946f74
Prerna <prerna181401@gamil.com>
2023-05-25 17:16:16 +0530

    basic styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import background from "./Artboard 1@10x.png";
-
+import background from "./rect.jpeg";
+import img from "../assets/Artboard.png";
 
 const Home = () => {
   return (
     <div
-      className="home w-full bg-cover h-screen mx-auto flex justify-center text-center flex-col font-sans "
-      style={{ backgroundImage: `url(${background})` }}
+      className="relative home w-full bg-center bg-cover h-screen m-0 flex justify-center text-center flex-col font-sans items-center "
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+      }}
     >
-      <div className="text-[#EAD3CB] text-2xl ">
-        Mapping Ancient and Historic Routes using Geo-Spatial Tools
+      <div className="relative bottom-28">
+        <img className="w-[41vw]" src={img} alt="image description" />
+        <h1 className="absolute font-bold text-[#845460] art top-32 right-32 -translate-x-1/2 -translate-y-1/4">
+          MARG
+        </h1>
       </div>
-      <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+      <div className="absolute  -translate-y-1/5 	">
+        <h2 className="relative text-[#EAD3CB] text-3xl ">
+          Mapping Ancient and Historic Routes using Geo-Spatial Tools
+        </h2>
 
-      <div className="text-lg text-[#BDC7C9]">
-        Funded By SHRI Science and Heritage Research Initiative, Department of
-        Science & Technology, GOI
+        <hr class="relative w-48 h-1 mx-auto my-4  bg-white border-0 rounded" />
+
+        <h3 className="relative text-base  text-[#BDC7C9]">
+          Funded By SHRI Science and Heritage Research Initiative, Department of
+          Science & Technology, GOI
+        </h3>
       </div>
     </div>
   );
